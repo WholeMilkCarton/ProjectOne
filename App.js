d77896abcd0d9ca8b9040d17cae2038277d42c73
WholeMilkCarton <orcinusorca1331@gmail.com>
2023-02-27 13:41:23 -0700

    checkpoint around goalInput handler

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  function goalInputHandler(enteredText) {
+    console.log(enteredText)
+  };
+
+  function addgoalHandler() {};
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal!' onChange={goalInputHandler} />
+          <Button title="Add Goal" />
+            </View>
+        <View style={styles.goalsContainer}>
+        <Text>List of Goals...</Text>
+       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space=between',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccccccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    margin: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
