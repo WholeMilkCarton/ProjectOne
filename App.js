0642cbfc90efb8b9594980a82376760080982d1b
WholeMilkCarton <orcinusorca1331@gmail.com>
2023-03-02 13:00:33 -0700

    project update

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  function goalInputHandler(enteredText) {
-    console.log(enteredText)
-  };
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
-  function addgoalHandler() {};
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+  }
 
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!' onChange={goalInputHandler} />
-          <Button title="Add Goal" />
-            </View>
-        <View style={styles.goalsContainer}>
-        <Text>List of Goals...</Text>
-       </View>
+        <TextInput
+          style={styles.textInput} 
+          placeholder="Your course goal!" 
+          onChangeText={goalInputHandler} 
+          />
+          <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+      </View>
     </View>
   );
 }
