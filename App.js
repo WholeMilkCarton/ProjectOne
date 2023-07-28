be3843d1df2805395e8a4a1ceb563b8ca8d4272e
WholeMilkCarton <orcinusorca1331@gmail.com>
2023-07-27 22:30:12 -0600

    initial commit

--- App.js
+++ App.js
@@ -1,38 +1,72 @@
 import { useState } from 'react';
-import { StyleSheet, View, Button, TextInput, FlatList } from 'react-native';
-
+import { StyleSheet, View, FlatList, Button } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import GoalItem from './components/GoalItem';
-import GoalInput from './componenets/GoalInput';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  //CourseGoals is the array.
+  const [modalIsVisible, setModalIsVisible] = useState(false);
   const [courseGoals, setCourseGoals] = useState([]);
+
+  function startAddGoalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function endAddGoalHandler() {
+    setModalIsVisible(false);
+  }
 
   function addGoalHandler(enteredGoalText) {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      //enteredGoalText comes from courseGoals array where the string of the goal comes from. you can add a key to this so you dont get warning.
       { text: enteredGoalText, id: Math.random().toString() },
     ]);
+    endAddGoalHandler();
   }
+
+  //for .filter below, goal represents all the items in that array. the .filter function says that if the argument is true, meaning if the id's do not match, it will be dropped. but if it is true, meaning they do not match, then that value will be included in the list. 
+  //the idea for the deleteGoalHandler is to first take the 2nd value from usestate from usestate(setCourseGoals), and reference that to
+  //delete a goal 
+  function deleteGoalHandler(id) {
+    setCourseGoals(currentCourseGoals => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
+  }
+
 //addGoalHandler in passed as a value to onAddGoal on the GoalInput and onAddGoal in forwarded to onPress in GoalInput.js and onPress in GoalInput.js calls the onAddGoal function to provice the enteredGoalText in the funciton addGoalHandler in App.js
   return (
-    <View style={styles.appContainer}>
-      <GoalInput onAddGoal={addGoalHandler} />
-      <View style={styles.goalsContainer}>
-        <FlatList 
-          data={courseGoals}
-          renderItem={(itemData) => {
-            itemData.index
-              return <GoalItem text={itemData.item.text} />;
-          }}
-          keyExtractor={(item, index) => {
-            return item.id;
-          }}
-          alwaysBounceVertical={false}
+    <>
+      <StatusBar style="light" />
+      <View style={styles.appContainer}>
+        <Button 
+          title='Add New Goal' 
+          color='#5e0acc' 
+          onPress={startAddGoalHandler}
         />
+        <GoalInput 
+          visible={modalIsVisible} 
+          onAddGoal={addGoalHandler} 
+          onCancel={endAddGoalHandler} />
+        <View style={styles.goalsContainer}>
+          <FlatList 
+            data={courseGoals}
+            renderItem={(itemData) => {
+                return (
+                  <GoalItem 
+                    text={itemData.item.text}
+                    id={itemData.item.id}
+                    onDeleteItem={deleteGoalHandler}
+                  />
+                );
+              }}
+            keyExtractor={(item, index) => {
+              return item.id;
+            }}
+            alwaysBounceVertical={false}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
